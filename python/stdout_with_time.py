c6f0b444959ce2d140fecc96fc15fcf328ed856a
Dmitry Kaluzhny <dmitryka@yandex-team.ru>
2016-01-23 19:35:21 +0300

    log to file

--- python/stdout_with_time.py
+++ python/stdout_with_time.py
@@ -4,20 +4,30 @@ from datetime import datetime
 
 class StdOutWithTime(object):
 
+    start = datetime.now()
+    file = open('log_%04d_%02d_%02d_%02d_%02d_%02d.txt'
+                % (start.year, start.month, start.day, start.hour, start.minute, start.second), 'w')
+
     def __init__(self, old):
         self.old = old
         self.new_line = True
 
     def write(self, x):
         if x == '\n':
-            self.old.write(x)
+            self.write2(x)
             self.new_line = True
         elif self.new_line:
-            self.old.write('%s: %s' % (str(datetime.now()), x))
+            self.write2('%s: %s' % (str(datetime.now()), x))
             self.new_line = False
         else:
-            self.old.write(x)
+            self.write2(x)
+
+    def write2(self, text):
+        self.old.write(text)
+        self.file.write(text)
+        self.file.flush()
 
     def flush(self):
         self.old.flush()
+        self.file.flush()
 
