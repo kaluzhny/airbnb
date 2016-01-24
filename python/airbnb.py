f44fdb73453c8536ec6334a90788bd231e11c766
Dmitry Kaluzhny <dmitryka@yandex-team.ru>
2016-01-24 14:17:35 +0300

    knn6 predict

--- python/airbnb.py
+++ python/airbnb.py
@@ -44,7 +44,7 @@ def run_airbnb(target):
 
     submission_file = os.path.join(
         submission_dir,
-        'submission_lr_knn_xg_rf_et_perm_' +
+        'submission_lr_knn6_xg_rf_et_perm_' +
         submission_suffix + '_seed_' + str(n_seed) + '.csv')
 
     def do_cross_validation():
@@ -72,4 +72,4 @@ def run_airbnb(target):
     elif target == 'prediction':
         make_prediction()
 
-run_airbnb('cv')
+run_airbnb('prediction')
