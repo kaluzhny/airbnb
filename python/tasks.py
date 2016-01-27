a53dafe3f9d4408f3d353778f2523534ca54f18d
Dmitry Kaluzhny <dmitryka@yandex-team.ru>
2016-01-27 20:04:10 +0300

    include secs elapsed

--- python/tasks.py
+++ python/tasks.py
@@ -40,7 +40,7 @@ def ds_from_df(data_df, sessions_df, is_test):
 
 
 def load_sessions(path):
-    return pd.read_csv(path, usecols=['user_id', 'action', 'action_type', 'action_detail', 'device_type'])
+    return pd.read_csv(path, usecols=['user_id', 'action', 'action_type', 'action_detail', 'device_type', 'secs_elapsed'])
 
 
 class Task(object):
@@ -60,7 +60,7 @@ class TrainingDataTask(Task):
 
     def load_train_data(self, sessions_df):
         data_df = read_from_csv(self.task_core.data_file, self.task_core.n_seed
-                                #, max_rows=50000
+                                #, max_rows=25000
                                 )
 
         cache_file = os.path.join(self.task_core.cache_dir, 'features_train_' + str(len(data_df.index)) + '.p')
@@ -86,7 +86,7 @@ class TrainingDataTask(Task):
 class TestDataTask(Task):
     def load_test_data(self, sessions_df):
         data_df = read_from_csv(self.task_core.test_data_file, self.task_core.n_seed
-                                #, max_rows=50000
+                                #, max_rows=25000
                                 )
 
         cache_file = os.path.join(self.task_core.cache_dir, 'features_test_' + str(len(data_df.index)) + '.p')
@@ -148,57 +148,36 @@ def run_model(x_train, y_train, x_test, classes_count, classifier, n_threads, n_
 
     classifiers_session_data, classifiers_no_session_data, classifiers_2014 = get_model_classifiers(n_threads, n_seed)
 
-    session_features_train, session_features_test = get_blend_features(
-        classifiers_session_data,
+    no_session_features_train, no_session_features_test = get_blend_features(
+        classifiers_no_session_data,
         classes_count,
-        x_train, y_train,
-        x_test,
+        remove_sessions_columns(x_train), y_train,
+        remove_sessions_columns(x_test),
         n_seed)
 
-    x_train = x_train.append_horizontal(session_features_train)
-    x_test = x_test.append_horizontal(session_features_test)
+    print('x_train: ', x_train.data_.shape)
+    print('x_test: ', x_test.data_.shape)
+    x_train_sessions, y_train_sessions, x_train_no_sessions, y_train_no_sessions = divide_by_has_sessions(
+        x_train, y_train)
+
+    print('x_train_sessions: ', x_train_sessions.data_.shape)
+    print('x_train_no_sessions: ', x_train_no_sessions.data_.shape)
+
+    session2014_features_train, session2014_features_test = get_blend_features(
+        classifiers_2014,
+        classes_count,
+        x_train_sessions, y_train_sessions, x_test,
+        n_seed)
+
+    x_train_sessions = x_train_sessions.append_horizontal(no_session_features_train.filter_rows_by_ids(x_train_sessions.ids_))
+    x_test = x_test.append_horizontal(no_session_features_test)
+
+    x_train_sessions = x_train_sessions.append_horizontal(session2014_features_train)
+    x_test = x_test.append_horizontal(session2014_features_test)
 
     print('Predicting all features...')
-    print_columns(x_train.columns_)
-    probabilities = simple_predict(clone(classifier), x_train, y_train, x_test)
-
-
-
-    #####################################################################################
-    #
-    # no_session_features_train, no_session_features_test = get_blend_features(
-    #     classifiers_no_session_data,
-    #     classes_count,
-    #     remove_sessions_columns(x_train), y_train,
-    #     remove_sessions_columns(x_test),
-    #     n_seed)
-
-    # print('x_train: ', x_train.data_.shape)
-    # print('x_test: ', x_test.data_.shape)
-    # x_train_sessions, y_train_sessions, x_train_no_sessions, y_train_no_sessions = divide_by_has_sessions(
-    #     x_train, y_train)
-
-    # print('x_train_sessions: ', x_train_sessions.data_.shape)
-    # print('x_train_no_sessions: ', x_train_no_sessions.data_.shape)
-    #
-    # session2014_features_train, session2014_features_test = get_blend_features(
-    #     classifiers_2014,
-    #     classes_count,
-    #     x_train_sessions, y_train_sessions, x_test,
-    #     n_seed)
-
-    # x_train_sessions = x_train_sessions.append_horizontal(session_features_train.filter_rows_by_ids(x_train_sessions.ids_))
-    # x_test = x_test.append_horizontal(session_features_test)
-    #
-    # x_train_sessions = x_train_sessions.append_horizontal(no_session_features_train.filter_rows_by_ids(x_train_sessions.ids_))
-    # x_test = x_test.append_horizontal(no_session_features_test)
-    #
-    # x_train_sessions = x_train_sessions.append_horizontal(session2014_features_train)
-    # x_test = x_test.append_horizontal(session2014_features_test)
-    #
-    # print('Predicting all features...')
-    # print_columns(x_train_sessions.columns_)
-    # probabilities = simple_predict(clone(classifier), x_train_sessions, y_train_sessions, x_test)
+    print_columns(x_train_sessions.columns_)
+    probabilities = simple_predict(clone(classifier), x_train_sessions, y_train_sessions, x_test)
 
     return probabilities
 
