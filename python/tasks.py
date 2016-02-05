3484b6a41614f813155993412afa5c57e26364fa
Dmitry Kaluzhny <dmitryka@yandex-team.ru>
2016-02-05 17:53:01 +0300

    removed ada-etc

--- python/tasks.py
+++ python/tasks.py
@@ -116,7 +116,7 @@ def get_model_classifiers(n_threads, n_seed):
     classifiers_session_data = [
         # (MultinomialNB(), True, False, 'nb'),
         # (LogisticRegression(), False, False, 'lr'),
-        (AdaBoostClassifier(base_estimator=ExtraTreesClassifier(random_state=n_seed), random_state=n_seed), False, False, 'adaetc_all'),
+        # (AdaBoostClassifier(base_estimator=ExtraTreesClassifier(random_state=n_seed), random_state=n_seed), False, False, 'adaetc_all'),
         (ExtraTreesClassifier(n_estimators=300, criterion='gini', n_jobs=2, random_state=n_seed), False, False, 'etc300_all'),
         (RandomForestClassifier(n_estimators=300, criterion='gini', n_jobs=2, random_state=n_seed), False, False, 'rfc300_all'),
         (XGBClassifier(objective='multi:softprob', max_depth=3, n_estimators=100, learning_rate=0.1, nthread=n_threads, seed=n_seed), False, False, 'xg3softprob100_all'),
@@ -126,7 +126,7 @@ def get_model_classifiers(n_threads, n_seed):
     classifiers_no_session_data = [
         # (MultinomialNB(), True, False, 'nb'),
         # (LogisticRegression(), False, False, 'lr'),
-        (AdaBoostClassifier(base_estimator=ExtraTreesClassifier(random_state=n_seed), random_state=n_seed), False, False, 'adaetc'),
+        # (AdaBoostClassifier(base_estimator=ExtraTreesClassifier(random_state=n_seed), random_state=n_seed), False, False, 'adaetc'),
         (RandomForestClassifier(n_estimators=300, criterion='gini', n_jobs=2, random_state=n_seed), False, False, 'rfc300'),
         (ExtraTreesClassifier(n_estimators=300, criterion='gini', n_jobs=2, random_state=n_seed), False, False, 'etc300'),
         (XGBClassifier(objective='multi:softprob', max_depth=3, n_estimators=100, learning_rate=0.1, nthread=2, seed=n_seed), False, False, 'xg3softprob100'),
@@ -134,7 +134,7 @@ def get_model_classifiers(n_threads, n_seed):
     ]
 
     classifiers_2014 = [
-        (AdaBoostClassifier(base_estimator=ExtraTreesClassifier(random_state=n_seed), random_state=n_seed), False, False, 'adaetc_2014'),
+        # (AdaBoostClassifier(base_estimator=ExtraTreesClassifier(random_state=n_seed), random_state=n_seed), False, False, 'adaetc_2014'),
         (RandomForestClassifier(n_estimators=300, criterion='entropy', n_jobs=2, random_state=n_seed), False, False, 'rfc300_e_2014'),
         (ExtraTreesClassifier(n_estimators=300, criterion='entropy', n_jobs=2, random_state=n_seed), False, False, 'etc300_e_2014'),
     ]
