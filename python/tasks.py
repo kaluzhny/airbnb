49b370dada03fc500c718aabb6d9fb8003f0f50f
Dmitry Kaluzhny <dmitryka@yandex-team.ru>
2016-02-10 03:32:05 +0300

    2014 lots blend

--- python/tasks.py
+++ python/tasks.py
@@ -171,13 +171,29 @@ def get_model_classifiers(n_threads, n_seed):
         (ExtraTreesClassifier(n_estimators=600, criterion='gini', n_jobs=n_threads, random_state=n_seed), False, False, 'etc600_2014'),
         (RandomForestClassifier(n_estimators=600, criterion='entropy', n_jobs=n_threads, random_state=n_seed), False, False, 'rfc600_e_2014'),
         (ExtraTreesClassifier(n_estimators=600, criterion='entropy', n_jobs=n_threads, random_state=n_seed), False, False, 'etc600_e_2014'),
+        (RandomForestClassifier(n_estimators=300, criterion='gini', n_jobs=n_threads, random_state=n_seed), False, False, 'rfc300_2014'),
+        (ExtraTreesClassifier(n_estimators=300, criterion='gini', n_jobs=n_threads, random_state=n_seed), False, False, 'etc300_2014'),
+        (RandomForestClassifier(n_estimators=300, criterion='entropy', n_jobs=n_threads, random_state=n_seed), False, False, 'rfc300_e_2014'),
+        (ExtraTreesClassifier(n_estimators=300, criterion='entropy', n_jobs=n_threads, random_state=n_seed), False, False, 'etc300_e_2014'),
+        (XGBClassifier(objective='multi:softprob', max_depth=2, n_estimators=100, learning_rate=0.1, nthread=n_threads, seed=n_seed), False, False, 'xg2softprob100_2014'),
         (XGBClassifier(objective='multi:softprob', max_depth=3, n_estimators=100, learning_rate=0.1, nthread=n_threads, seed=n_seed), False, False, 'xg4softprob100_2014'),
+        (XGBClassifier(objective='multi:softprob', max_depth=5, n_estimators=100, learning_rate=0.1, nthread=n_threads, seed=n_seed), False, False, 'xg5softprob100_2014'),
+        (XGBClassifier(objective='multi:softprob', max_depth=3, n_estimators=100, learning_rate=0.1, nthread=n_threads, seed=n_seed), False, True, 'xg3softprob100log_2014'),
+        (KNeighborsClassifier(n_neighbors=16, p=1, n_jobs=n_threads), False, True, 'knn_16p1_2014'),
         (KNeighborsClassifier(n_neighbors=32, p=1, n_jobs=n_threads), False, True, 'knn_32p1_2014'),
         (KNeighborsClassifier(n_neighbors=64, p=1, n_jobs=n_threads), False, True, 'knn_64p1_2014'),
         (KNeighborsClassifier(n_neighbors=128, p=1, n_jobs=n_threads), False, True, 'knn_128p1_2014'),
         (KNeighborsClassifier(n_neighbors=256, p=1, n_jobs=n_threads), False, True, 'knn_256p1_2014'),
         (KNeighborsClassifier(n_neighbors=512, p=1, n_jobs=n_threads), False, True, 'knn_512p1_2014'),
         (KNeighborsClassifier(n_neighbors=1024, p=1, n_jobs=n_threads), False, True, 'knn_1024p1_2014'),
+        (KNeighborsClassifier(n_neighbors=16, p=2, n_jobs=n_threads), False, True, 'knn_16p2_2014'),
+        (KNeighborsClassifier(n_neighbors=32, p=2, n_jobs=n_threads), False, True, 'knn_32p2_2014'),
+        (KNeighborsClassifier(n_neighbors=64, p=2, n_jobs=n_threads), False, True, 'knn_64p2_2014'),
+        (KNeighborsClassifier(n_neighbors=128, p=2, n_jobs=n_threads), False, True, 'knn_128p2_2014'),
+        (KNeighborsClassifier(n_neighbors=256, p=2, n_jobs=n_threads), False, True, 'knn_256p2_2014'),
+        (KNeighborsClassifier(n_neighbors=512, p=2, n_jobs=n_threads), False, True, 'knn_512p2_2014'),
+        (KNeighborsClassifier(n_neighbors=1024, p=2, n_jobs=n_threads), False, True, 'knn_1024p2_2014'),
+        (KNeighborsClassifier(n_neighbors=2048, p=2, n_jobs=n_threads), False, True, 'knn_2048p2_2014'),
     ]
 
     return classifiers_session_data, classifiers_no_session_data, classifiers_2014
@@ -234,7 +250,7 @@ def run_model(x_train, y_train, x_test, classes_count, classifier, n_threads, n_
     bag = BaggingClassifier(base_estimator=xgb, n_estimators=3, random_state=n_seed, verbose=10)
 
     print('calculating cv...')
-    do_cv(remove_sessions_has_action_detail_columns(x_train).data_, y_train, xgb, 3)
+    do_cv(x_train.data_, y_train, xgb, 3)
 
     probabilities = simple_predict(bag, x_train, y_train, x_test)
 
